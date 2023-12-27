b3b7958ede3a9fd895977e327aeebb46b4ea0efc
bhavukkalra <bhavukkalra1786@gmail.com>
2023-12-27 14:26:50 +0530

    TODO left button should be visible now on the youtibe player

--- background.js
+++ background.js
@@ -1,4 +1,3 @@
-// import chrome from "./chrome-extension.iml"
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
     if (tab.url && tab.url.includes("youtube.com/watch")) {
         const queryParameters = tab.url.split("?")[1];
--- contentScript.js
+++ contentScript.js
@@ -15,6 +15,23 @@
         const bookmarkBtnExists = document.getElementsByClassName("bookmark-name")[0];
 
         console.log(bookmarkBtnExists);
+
+        if(!bookmarkBtnExists){
+            const bookmarkBtn = document.createElement("img");
+            // From where does this url fetches from
+            bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png")
+            bookmarkBtn.className = "ytp-button " + "bookmark-btn";
+            bookmarkBtn.title = "Click to bookmark the current timestamp"
+
+
+            youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
+            youtubePlayer = document.getElementsByClassName("video-stream")[0];
+
+            // Append the button to the left row
+            youtubeLeftControls.appendChild(bookmarkBtn);
+        }
     }
+    // Fix this at a later point(due to onUpdate event listener) TODO
+    newVideoLoaded();
     
 })();
