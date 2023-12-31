ed3bc0e47020f1bed36b45709647290c9f36ecf8
bhavukkalra <bhavukkalra1786@gmail.com>
2023-12-31 19:37:07 +0530

    dynamically fetch the current time on the player added

--- contentScript.js
+++ contentScript.js
@@ -2,6 +2,7 @@
     let youtubeLeftControls = ""
     let youtubePlayer = "";
     let currentVideo = ""
+    let currentVideoBookmarks = [];
 
     chrome.runtime.onMessage.addListener((obj, sender, response) => {
         const {type, videoId} = obj;
@@ -19,7 +20,8 @@
     }
     
     const newVideoLoaded = () => {
-        const bookmarkBtnExists = document.getElementsByClassName("bookmark-name")[0];
+        // Condition always failing and more elements are getting appended TODO
+        const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
 
         console.log(bookmarkBtnExists);
 
@@ -33,13 +35,49 @@
 
             youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
             youtubePlayer = document.getElementsByClassName("video-stream")[0];
-            console.log(`The left controls are ${youtubeLeftControls}`);
-            console.log(`The youtube player is ${youtubePlayer}`);
+            console.log(`The left controls are`);
+            console.log(youtubeLeftControls);
+
+            console.log(`The youtube player is`);
+            console.log(youtubePlayer);
             // Append the button to the left row
             youtubeLeftControls.appendChild(bookmarkBtn);
+            bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
+
         }
     }
     // Fix this at a later point(due to onUpdate event listener) TODO
-    callWithADelay();
+    callWithADelay(); // (Two time showing)
+    const addNewBookmarkEventHandler = () => {
+        const timeStampElement = document.getElementsByClassName("ytp-time-current")[0];
+        const timeStampFromYoutubeUIString = timeStampElement.innerHTML;
+
+        const currentTime = youtubePlayer.currentTime;
+        console.log(`The currentTime is - ${currentTime}`)
+
+        // const getYoutubeTIme = getTime(currentTime);
+
+
+        // console.log(`Output from the getTime fx - ${getYoutubeTIme}`)
+        const newBookmark =  {
+            time: currentTime,
+            desc: `Bookmark at ${timeStampFromYoutubeUIString}`
+        }
+        console.log(`New bookmark - ${JSON.stringify(newBookmark)}`);
+        console.log(newBookmark);
+
+        chrome.storage.sync.set({
+            [currentVideo]: JSON.stringify([...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time))
+        });
+    }
+
+    // const getTime = timeInput => {
+    //     let date = new Date(0);
+    //     date.setSeconds(timeInput);
+    //
+    //     console.log(`date varibale - ${date}`);
+    //
+    //         return date.toISOString().substring(8, 11);
+    // }
     
 })();
