a0b67d61784937dc0b20ab5ae58fa4e6033c46d3
bhavukkalra <bhavukkalra1786@gmail.com>
2024-01-01 18:00:57 +0530

    Get active tab and change the extension screen based on the currentActive Tab

--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
+import { getActiveTabURL } from './utils.js';
+
 // adding a new bookmark row to the popup
 const addNewBookmark = () => {};
 
@@ -9,4 +11,27 @@ const onDelete = e => {};
 
 const setBookmarkAttributes =  () => {};
 
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", async () => {
+    const activeTab = await getActiveTabURL();
+    const queryParameters = activeTab.url.split("?")[1];
+    const urlParameters = new URLSearchParams(queryParameters);
+
+    const currentVideo = urlParameters.get("v");
+
+    // why [currentVideo] as a parameter??
+    if(activeTab.url.includes("youtube.com/watch") && currentVideo){
+        chrome.storage.sync.get([currentVideo], (data) => {
+            console.log("it returns a promise");
+            console.log(data);
+            const currentVideoBookmarks = data[currentVideo] ? JSON.parse(data[currentVideo]) : [];
+
+        //     viewBookmarks
+        })
+    }else{
+        // Not a youtube video page
+        const container = document.getElementsByClassName("container")[0];
+        container.innerHTML = '<div class="title"> This is not a youtube page</div>>'
+
+    }
+
+});
